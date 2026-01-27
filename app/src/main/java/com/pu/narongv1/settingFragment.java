006ea9147bc6713301e9b38d310069ae4d79131b
Mangop-hub <nrngkhchaykaewkanha@gmail.com>
2026-01-27 15:10:15 +0700

    V1.0.3

--- app/src/main/java/com/pu/narongv1/settingFragment.java
+++ app/src/main/java/com/pu/narongv1/settingFragment.java
@@ -1,15 +1,32 @@
 package com.pu.narongv1;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 
 public class settingFragment extends Fragment {
+
+    private int locationRequestCode = 1000;
+    private Button button;
+    private SupportMapFragment mapFragment;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private double[] clat = {0};
+    private double[] clng = {0};
 
 
     private static final String ARG_PARAM1 = "param1";
@@ -34,6 +51,29 @@ public class settingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View view = inflater.inflate(R.layout.fragment_setting, container, false);
+        button = (Button) view.findViewById(R.id.button);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        return view;
+    }
+    private void Get_Current_location() {
+        if (ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getActivity(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            clat[0] = location.getLatitude();
+                            clng[0] = location.getLongitude();
+
+                        }
+                    }
+                });
     }
 }
