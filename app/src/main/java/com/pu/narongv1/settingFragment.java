e3aa77cf0f661db77374920b7ff200e6db05b7c9
Mangop-hub <nrngkhchaykaewkanha@gmail.com>
2026-02-03 15:17:06 +0700

    V1.0.4

--- app/src/main/java/com/pu/narongv1/settingFragment.java
+++ app/src/main/java/com/pu/narongv1/settingFragment.java
@@ -2,6 +2,8 @@ package com.pu.narongv1;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 
@@ -12,6 +14,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -25,16 +30,23 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.io.IOException;
+import java.util.List;
+
+
 
 public class settingFragment extends Fragment {
 
     private int locationRequestCode = 1000;
-    private Button button;
+    private Button button,button2;
     private SupportMapFragment mapFragment;
     private FusedLocationProviderClient mFusedLocationClient;
     private double[] clat = {0};
     private double[] clng = {0};
 
+    EditText editTextname;
+
+    private GoogleMap mMap;
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -62,6 +74,8 @@ public class settingFragment extends Fragment {
         button = (Button) view.findViewById(R.id.button);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
         mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        editTextname = view.findViewById(R.id.editTextname);
+        button2 = view.findViewById(R.id.button2);
         if (ActivityCompat.checkSelfPermission(getActivity(),
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(getActivity(),
@@ -84,6 +98,57 @@ public class settingFragment extends Fragment {
                         Toast.LENGTH_SHORT).show();
             }
         });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mapFragment != null) {
+                    String location = editTextname.getText().toString();
+                    List<Address> addressList = null;
+                    // checking if the entered location is null or not.
+                    if (location != null || location.equals("")) {
+                        if(mMap != null) {
+                            mMap.clear();
+                        }
+                        // on below line we are creating and initializing a geo coder.
+                        Geocoder geocoder = new Geocoder(getActivity());
+                        try {
+                            // on below line we are getting location from the
+                            // location name and adding that location to address list.
+                            addressList = geocoder.getFromLocationName(location, 1);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        // on below line we are getting the location
+                        // from our list a first position.
+                        Address address = addressList.get(0);
+
+                        // on below line we are creating a variable for our location
+                        // where we will add our locations latitude and longitude.
+                        LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+                        clat[0] = address.getLatitude();
+                        clng[0] = address.getLongitude();
+                        String strsnippet =  location + "Here at " + clat[0] + ", " + clng[0];
+
+                        MarkerOptions options1 = new MarkerOptions()
+                                .position(latLng)
+                                .title(location)
+                                .snippet(strsnippet)
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+
+                        // on below line we are adding marker to that position.
+                        mMap.addMarker(options1);
+
+                        // below line is to animate camera to that position.
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+                    }
+                }
+                Toast.makeText(getActivity(), "Get Your Location", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return view;
     }
     private void Get_Current_location() {
@@ -122,13 +187,14 @@ public class settingFragment extends Fragment {
             LatLng cposition2 = new LatLng(35.683575632775074, 139.75819730549824);
             MarkerOptions options2 = new MarkerOptions()
                     .position(cposition2)
-                    .title("Maker")
+                    .title("Maker 1")
                     .snippet("Home")
                     .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
 
             googleMap.addMarker(options);
             googleMap.addMarker(options2);
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(cposition, 12));
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(cposition, 15));
+            mMap = googleMap;
         }
     };
 
